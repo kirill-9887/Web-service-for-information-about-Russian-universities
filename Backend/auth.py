8d4438dd3c637c44106d2c42bcba6db05b920630
kirill-9887 <>
2025-03-16 20:12:40 +0300

    Added functions for authorization: token generation and verification, password hashing and password verification

---
+++ Backend/auth.py
@@ -0,0 +1,49 @@
+import time
+import secrets
+import argon2
+from database import create_db_session
+import db_tables as dbt
+
+
+TOKEN_LENGTH = 32  # в байтах
+TOKEN_TIME = 24 * 60 * 60
+
+
+def generate_session_token(length=TOKEN_LENGTH):
+    token = secrets.token_hex(length)
+    return token
+
+
+def hash_password(password):
+    ph = argon2.PasswordHasher()
+    password_hash = ph.hash(password)
+    return password_hash
+
+
+def verify_password(password_hash, password) -> bool:
+    ph = argon2.PasswordHasher()
+    try:
+        ph.verify(password_hash, password)
+        return True
+    except argon2.exceptions.VerifyMismatchError:
+        return False
+
+
+def verify_token(token):
+    """Возвращает id пользователя, если токен действителен, иначе None"""
+    token_hash = hash_password(token)
+    db_session = create_db_session()
+    try:
+        session = db_session.query(dbt.Session).filter_by(token_hash=token_hash).first()
+        if not session:
+            return None
+        current_time = int(time.time())
+        if current_time > session.expiration_time:
+            session.is_active = 0
+            db_session.commit()
+            return None
+        if not session.is_active:
+            return None
+        return session.user_id
+    finally:
+        db_session.close()
