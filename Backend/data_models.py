2fe666c39752bc3b614c17fe6c451fb09a40412e
kirill-9887 <>
2025-03-20 00:04:49 +0300

    Added pydantic models for data exchange between client and server

--- Backend/data_models.py
+++ Backend/data_models.py
@@ -1,3 +1,64 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
 ADMIN_ACCESS = 1
 EDITOR_ACCESS = 2
 READER_ACCESS = 3
+
+
+class UserRegData(BaseModel):
+    username: str
+    name: str
+    surname: str
+    patronymic: str
+    password: str
+
+
+class RegResult(BaseModel):
+    status_ok: bool
+    token: Optional[str] = None
+    error: Optional[str] = None
+
+
+class LoginResult(BaseModel):
+    status_ok: bool
+    token: Optional[str] = None
+    error: Optional[str] = None
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+class SessionToken(BaseModel):
+    token: str
+
+
+class LogoutResult(BaseModel):
+    status_ok: bool = True
+    error: Optional[str] = None
+
+
+class ChangePasswordData(BaseModel):
+    token: str
+    password: str
+    new_password: str
+    repeated_password: str
+
+
+class ChangePasswordResult(BaseModel):
+    status_ok: bool
+    error: Optional[str] = None
+
+
+class ChangeAccessData(BaseModel):
+    token: str
+    username: str
+    new_access_level: int
+
+
+class BaseResult(BaseModel):
+    status_ok: bool
+    error: Optional[str] = None
