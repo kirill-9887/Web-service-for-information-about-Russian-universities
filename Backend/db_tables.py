8412c93cd19856e8eaece5df01e1233a74003268
kirill-9887 <>
2025-04-04 22:41:11 +0300

    Added relationships between tables for sqlalchemy

--- Backend/db_tables.py
+++ Backend/db_tables.py
@@ -7,6 +7,7 @@ from sqlalchemy import event
 import uuid
 import datetime
 import pytz
+from sqlalchemy.orm import relationship
 
 import auth
 from database import Base, engine
@@ -63,7 +64,9 @@ class University(Base):
     type_name = Column(TEXT)
     region_name = Column(TEXT)
     federal_district_name = Column(TEXT)
-
+    head_edu_org = relationship("University", back_populates="branches", remote_side=[id])
+    branches = relationship("University", back_populates="head_edu_org")
+    eduprogs = relationship("EduProg", back_populates="university")
 
 class NotUnivException(Exception):
     def __str__(self):
@@ -94,6 +97,7 @@ class EduProg(Base):
     is_canceled = Column(TEXT)
     is_suspended = Column(TEXT)
     university_id = Column(String, ForeignKey("universities.id", ondelete="CASCADE"))
+    university = relationship("University", back_populates="eduprogs")
 
 
 create_tables()
