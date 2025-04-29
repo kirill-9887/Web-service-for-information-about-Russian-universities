c3d48f2466e242c9f438daae6d08188ea176fd47
kirill-9887 <>
2025-04-30 01:55:17 +0300

    Added configure data

--- Backend/config.py
+++ Backend/config.py
@@ -1,1 +1,3 @@
-DATABASE_URL = "sqlite:///../Database/database.sqlite3"
+DATABASE_URL = "sqlite:///../Database/database22.sqlite3"
+DEFAULT_PAGE_SIZE = 40
+DATAFILENAME = "../Downloads/data-20250427-structure-20160713.xml"
