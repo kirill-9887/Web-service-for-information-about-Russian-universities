5b09d8a3ddc6a63042cc9b8513a5283ba8d60f94
kirill-9887 <>
2025-05-03 02:56:28 +0300

    Added DB_ECHO flag in config.py

--- Backend/config.py
+++ Backend/config.py
@@ -1,5 +1,6 @@
 DATABASE_URL = "sqlite:///../Database/database22.sqlite3"
 DEFAULT_PAGE_SIZE = 40
 DATAFILENAME = "../Downloads/data-20250427-structure-20160713.xml"
+DB_ECHO = False
 RESOURCES_RELATIVE_CATALOG = "../Frontend/"
 DISABLE_FOREIGN_KEY_CONSTRAINT = True
