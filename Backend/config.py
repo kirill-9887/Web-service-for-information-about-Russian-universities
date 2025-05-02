f0b9028ea1bad7cb097f16190127350a8474f39e
kirill-9887 <>
2025-05-02 15:35:04 +0300

    Added the ability to enable foreign key verification. It can be disabled when updating data from the registry.

--- Backend/config.py
+++ Backend/config.py
@@ -1,3 +1,4 @@
 DATABASE_URL = "sqlite:///../Database/database22.sqlite3"
 DEFAULT_PAGE_SIZE = 40
 DATAFILENAME = "../Downloads/data-20250427-structure-20160713.xml"
+DISABLE_FOREIGN_KEY_CONSTRAINT = True
--- Backend/database.py
+++ Backend/database.py
@@ -1,9 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import sqlalchemy
+from sqlalchemy.orm import declarative_base, sessionmaker
+import sqlite3
 import config
 
 
 engine = create_engine(config.DATABASE_URL, echo=True)
+@sqlalchemy.event.listens_for(sqlalchemy.engine.Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if config.DISABLE_FOREIGN_KEY_CONSTRAINT:
+        return
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
 Base = declarative_base()
 DBSession = sessionmaker(bind=engine)
 
